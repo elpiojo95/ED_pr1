8e0a34f7a7bb68fa848a5471d180ecdaa35d1cea
elpiojo95 <leo1ragomez95@gmail.com>
2020-03-03 01:29:36 +0100

    implementado juego de pruebas

--- src/test_pila.c
+++ src/test_pila.c
@@ -2,10 +2,49 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+void estadopila(Pila p)
+{
+    if (EsBuida(p)) printf("La pila esta VACIA\n");
+    if (EsPlena(p)) printf("La pila esta LLENA\n");
+}
+
 int main()
 {
-    Pila p;
-    p = Crear(10);
-    printf("holi");
+    printf("INICIO DE TEST DE PRUEBAS\n\n");
+    int numeros[] = {2,53,127,199,283,383,467,577,661,769};
+
+    /* CREAR PILA */
+    printf("Creando pila...\n");
+    Pila p = Crear(10);
+    printf("Pila creada!\n");
+    estadopila(p);
+
+    /* RELLENAR PILA */
+    printf("\nLlenando pila...\n");
+    for (int i = 0; i < 10; i++) Apilar(&p, numeros[i]);
+    printf("La pila se ha llenado!\n");
+    estadopila(p);
+    Mostrar(p);
+
+    /* VER CIMA */
+    printf("\nLa Cima es: %d\n", Cim(p));
+    estadopila(p);
+
+    /* DESAPILAR */
+    printf("\nVaciando la pila...\n");
+    for (int i = 0; i < 5; i++) Desapilar(&p);
+    estadopila(p);
+    Mostrar(p);
+    for (int i = 0; i < 5; i++) Desapilar(&p);
+    printf("Pila Vaciada!\n");
+    estadopila(p);
+    Mostrar(p);
+    /* DESTRUIR */
+    printf("\nDestruyendo la pila...\n");
+    Destruir(&p);
+    printf("Pila destruida!\n");
+
+
+    printf("\n\tEl test ha sido un exito!\n");
     return 0;
 }
