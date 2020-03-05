fbb582894d339c78760c03e860e82920c6c3f079
elpiojo95 <leo1ragomez95@gmail.com>
2020-03-05 09:21:41 +0100

    create function for read a file

--- src/rpn.c
+++ src/rpn.c
@@ -1,10 +1,24 @@
 #include "pila.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+
+int LeerFichero(const char *fileName, char **s);
 
 int main()
 {
-    FILE *file = fopen("input.txt","r");
+    char *data;
+    int nElem;
+    nElem = LeerFichero("input.txt", &data);
+    printf("size: %d, string: %s", nElem, data);
+
+    return 0;
+}
+
+
+int LeerFichero(const char *fileName, char **s)
+{
+    FILE *file = fopen(fileName,"r");
     int size;
 
     if (file == NULL)
@@ -12,10 +26,22 @@ int main()
         printf("File does not exist!\n");
         exit(EXIT_FAILURE);
     }
-    fseek(file, 0, SEEK_END); // seek to end of file
-    size = ftell(file); // get current file pointer
-    fseek(file, 0, SEEK_SET); // seek back to beginning of file
+    /* Get string size from file */
+    fseek(file, 0, SEEK_END);
+    size = ftell(file);
+    fseek(file, 0, SEEK_SET);
+
+    /* Allocate memory & full string */
+    *s = (char*) malloc(sizeof(char)*size);
+    if (s == NULL)
+    {
+        exit(EXIT_FAILURE);
+    }
+    
+    for (int i = 0; i < size; i++)
+    {
+        (*s)[i] = fgetc(file);
+    }
     fclose(file);
-    printf("size: %d", size);
-    return 0;
+    return size;
 }
