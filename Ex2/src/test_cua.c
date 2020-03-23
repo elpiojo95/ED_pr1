ae8b149656c017abb81165a02c9adeba6e4653cd
elpiojo95 <leo1ragomez95@gmail.com>
2020-03-23 14:29:11 +0100

    test

--- Ex2/src/test_cua.c
+++ Ex2/src/test_cua.c
@@ -1,61 +1,115 @@
 #include <stdio.h>
 #include "cua.h"
 
-#define MAX 15
-
 int main(int argc, char *argv[]) {
-	Cua c;
-	int error = 0, top;
-	error = Crear(&c, MAX);
-	if (error)
-	{
-		printf("EROOOOOOR crear: %d \n", error);
-	}
-	for (int i = 0; i < MAX; i++)
-	{
-		error = Encuar(&c, i+1);
-		if (error)
-		{
-			printf("Error Encuar: %d\n", error);
-		}
-	}
-	Mostrar(c);
-	error = Cap(c, &top);
-	if (error)
-	{
-		printf("Error Cap: %d\n", error);
-	}
-	printf("%d\n", top);
+	Cua c = NULL, c_nocreada = NULL;
+	int error = 0, num;
+	bool b;
 
-	error = Desencuar(&c);
-	error = Desencuar(&c);
-	error = Desencuar(&c);
-	error = Desencuar(&c);
-	error = Desencuar(&c);
-	error = Desencuar(&c);
-	error = Encuar(&c, 22);
-	if (error)
-	{
-		printf("Error Desencuar: %d\n", error);
-	}
-	error = Cap(c, &top);
-	if (error)
-	{
-		printf("Error Cap: %d\n", error);
-	}
+	/* Test 1 */
+	error = Crear(&c, 1);
+	if (error == SUCCESS) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 1\n");
 
-	printf("%d\n", top);
-	Mostrar(c);
+	/* Test 2 */
+	error = Crear(&c, -1);
+	if (error == ERROR_CREAR) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 2\n");
+
+	/* Test 3 */
+	error = Crear(&c, 1);
+	error = EsBuida(c_nocreada, &b);
+	if (error == CUA_NO_CREADA) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 3\n");
+	
+	/* Test 4 */
+	error = EsBuida(c, &b);
+	if (b == true) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 4\n");
+
+	/* Test 5 */
+	error = Encuar(&c, 1);
+	error = EsBuida(c, &b);
+	if (b == false) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 5\n");
+
+	/* Test 6 */
+	error = Crear(&c, 1);
+	error = EsPlena(c_nocreada, &b);
+	if (error == CUA_NO_CREADA) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 6\n");
+	
+	/* Test 7 */
+	error = EsPlena(c, &b);
+	if (b == false) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 7\n");
+
+	/* Test 8 */
+	error = Encuar(&c, 1);
+	error = EsPlena(c, &b);
+	if (b == true) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 8\n");
 	error = Destruir(&c);
-	if (error)
-	{
-		printf("Error Destruir: %d\n", error);
-	}
-	printf("\npost destruir\n");
-	error = Mostrar(c);
-	if (error)
-	{
-		printf("Error Mostrar: %d\n", error);
-	}
+	
+	/* Test 9 */
+	error = Crear(&c, 1);
+	error = Encuar(&c, 1);
+	if (error == SUCCESS) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 9\n");
+
+	/* Test 10 */
+	error = Encuar(&c, 1);
+	if (error == CUA_PLENA) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 10\n");
+
+	/* Test 11 */
+	error = Desencuar(&c);
+	if (error == SUCCESS) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 11\n");
+
+	/* Test 12 */
+	error = Desencuar(&c);
+	if (error == CUA_BUIDA) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 12\n");
+
+	/* Test 13 */
+	error = Encuar(&c, 22);
+	error = Cap(c, &num);
+	if ((error == SUCCESS) && (num = 22)) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 13\n");
+
+	/* Test 14 */
+	error = Desencuar(&c);
+	error = Cap(c, &num);
+	if ((error == CUA_BUIDA)) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 14\n");
+
+	/* Test 15 */
+	error = Destruir(&c);
+	if ((error == SUCCESS) && (c = NULL)) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 15\n");
+
+	/* Test 16 */
+	error = Destruir(&c);
+	if (error == ERROR_DESTRUIR) printf("[ok]");
+	else printf("[  ]");
+	printf(" Test Crear 16\n");
+	//mostrar
+
 	return(0);
 }
