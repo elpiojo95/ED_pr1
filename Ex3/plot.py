e02ad49d19fd89c640f32a778d6412ba616a5aec
elpiojo95 <leo1ragomez95@gmail.com>
2020-04-13 19:04:05 +0200

    plot de datos

---
+++ Ex3/plot.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x, y, e = np.loadtxt('data.csv', delimiter=';', unpack=True)
+
+plt.errorbar(x, y, e, linestyle='None', marker='None', label='Error')
+plt.plot(x,y, label='Media')
+
+plt.xlabel('Cantidad de numeros')
+
+plt.xticks(np.arange(0, 50001, 5000))
+
+plt.ylabel('Coste de busqueda')
+plt.title('Grafico de costes de lista linkada')
+plt.legend(loc='upper left')
+plt.show()
